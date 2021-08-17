038759a4c1866a2a7cb1d5db1133b40874adf36e
basti <bastian.kubaile01@gmail.com>
2021-08-17 11:03:00 +0200

    Changed ordering of nested Loops in DifferenceCalculator. Added Logic in CaptchaSolver to go from caclulated Differnce to string of captcha.

--- src/CaptchaSolver.py
+++ src/CaptchaSolver.py
@@ -1,16 +1,51 @@
+import functools
 from src.ImageReader import read_image
 from src.LetterStore import LetterStore
-from src.DifferenceCalculator import get_minimum_difference
+from src.DifferenceCalculator import get_differences, DifferencePositionList
+from typing import List, Tuple
+
+LetterFits = Tuple[float, int, str]
+
 
 class CaptchaSolver:
 
-    def __init__(self, letter_store: LetterStore):
+    def __init__(self,
+                 letter_store: LetterStore,
+                 letters_per_captcha: int = 6,
+                 possible_letter_duplicates_per_captcha: int = 2):
         self.letter_store: LetterStore = letter_store
+        self.possible_duplicates: int = possible_letter_duplicates_per_captcha
+        self.letters_per_captcha: int = letters_per_captcha
 
-    def parse_image(self, img_path: str):
+    def parse_image(self, img_path: str) -> str:
         captcha_image = read_image(img_path)
+        all_positions: List[LetterFits] = []
 
         for letter in self.letter_store.get_letters():
-            for letter_image in self.letter_store.get_images(letter):
-                get_minimum_difference(captcha_image, letter_image)
 
+            all_differences: DifferencePositionList = []
+            for letter_image in self.letter_store.get_images(letter):
+                all_differences = sorted(get_differences(captcha_image, letter_image) + all_differences)
+
+            useful_positions = self._find_useful_differences_positions(all_differences, letter_image.shape[1])
+            to_append: List[LetterFits] = list(map(lambda x: (x[0], x[1], letter), useful_positions))
+            all_positions = sorted(to_append + all_positions)
+        most_relevant_positions = all_positions[0: self.letters_per_captcha]
+        sorted_by_x_offset = sorted(most_relevant_positions, key=lambda x:  x[1])
+        captcha_string = functools.reduce(lambda reduced_value, position: reduced_value + position[2],
+                                          sorted_by_x_offset, initial="")
+        return captcha_string
+
+    def _find_useful_differences_positions(self, raw_differences: DifferencePositionList, letter_width: int):
+        useful_positions: DifferencePositionList = []
+        while len(useful_positions) < self.possible_duplicates and raw_differences:
+            curr_difference_position = raw_differences.pop(0)
+            is_covered = False
+            for i in range(0, len(useful_positions)):
+                x_offset = useful_positions[i][1]
+                if x_offset - letter_width <= curr_difference_position[1] <= x_offset + letter_width:
+                    is_covered = True
+                    break
+            if not is_covered:
+                useful_positions.append(curr_difference_position)
+        return useful_positions
--- src/DifferenceCalculator.py
+++ src/DifferenceCalculator.py
@@ -1,13 +1,24 @@
 from numpy import ndarray, zeros, matmul, subtract, absolute, sum, identity, iinfo
 import numpy as np
+import bisect
+from typing import List, Tuple
 
 
-def get_minimum_difference(captcha_image: ndarray, letter_image: ndarray) -> (float, int):
+DifferencePositionList = List[Tuple[float, int]]
+
+
+def get_differences(captcha_image: ndarray, letter_image: ndarray) -> DifferencePositionList:
+    """ Returns a sorted List containing all possible DifferencePositions where the letter could be placed inside the Image.
+    Each DifferencePosition contains a float between 0 and 1 describing how much the image fits at the position
+    and the x value at which the image was fitted.
+    The List is sorted by the difference value."""
+
     if letter_image.shape > captcha_image.shape:
         raise ValueError("The letter image is bigger than the captcha image")
     if not captcha_image.dtype == letter_image.dtype:
         raise ValueError("The Matrices representing both images must be of the same data type")
 
+    positions_to_return: DifferencePositionList = []
     data_type = captcha_image.dtype
 
     l_h, l_w = letter_image.shape[0], letter_image.shape[1]  # Letter height and width
@@ -20,33 +31,32 @@ def get_minimum_difference(captcha_image: ndarray, letter_image: ndarray) -> (fl
     width_mask = zeros((i_w, i_w), dtype=data_type)
     height_mask = zeros((i_h, i_h), dtype=data_type)
 
-    minimum_difference = 1.0
-    at_x_offset = -1
-    for y_offset in range(0, height_difference + 1):
-        height_normalisation_matrix.fill(0)
-        height_normalisation_matrix[y_offset:y_offset + l_h, 0:l_h] = identity(l_h, dtype=data_type)
+    for x_offset in range(0, width_difference + 1):
+        width_normalisation_matrix.fill(0)
+        width_normalisation_matrix[0:l_w, x_offset:x_offset + l_w] = identity(l_w, dtype=data_type)
 
-        height_mask.fill(0)
-        height_mask[y_offset: l_h + y_offset, y_offset: l_h + y_offset] = identity(l_h, dtype=data_type)
-        for x_offset in range(0, width_difference + 1):
-            width_normalisation_matrix.fill(0)
-            width_normalisation_matrix[0:l_w, x_offset:x_offset+l_w] = identity(l_w, dtype=data_type)
+        width_mask.fill(0)
+        width_mask[x_offset:x_offset + l_w, x_offset: x_offset + l_w] = identity(l_w, dtype=data_type)
+        min_difference = 1.0
+        for y_offset in range(0, height_difference + 1):
+            height_normalisation_matrix.fill(0)
+            height_normalisation_matrix[y_offset:y_offset + l_h, 0:l_h] = identity(l_h, dtype=data_type)
+
+            height_mask.fill(0)
+            height_mask[y_offset: l_h + y_offset, y_offset: l_h + y_offset] = identity(l_h, dtype=data_type)
 
             width_normalized = matmul(letter_image, width_normalisation_matrix)
             normalized_letter = matmul(height_normalisation_matrix, width_normalized)
 
-            width_mask.fill(0)
-            width_mask[x_offset:x_offset + l_w, x_offset: x_offset + l_w] = identity(l_w, dtype=data_type)
             width_masked_image = matmul(captcha_image, width_mask)
-
             masked_image = matmul(height_mask, width_masked_image)
+
             current_difference = _calculate_difference(masked_image, normalized_letter, l_w, l_h, data_type)
+            if current_difference < min_difference:
+                min_difference = current_difference
+        bisect.insort(positions_to_return, (min_difference, x_offset))
 
-            if current_difference < minimum_difference:
-                minimum_difference = current_difference
-                at_x_offset = x_offset
-
-    return minimum_difference, at_x_offset
+    return positions_to_return
 
 def _calculate_difference(masked_captcha_image: ndarray, normalized_letter_image: ndarray, letter_width: int, letter_height: int, data_type):
     max_data_value = iinfo(data_type).max
@@ -54,6 +64,5 @@ def _calculate_difference(masked_captcha_image: ndarray, normalized_letter_image
 
     max_pixel_values = np.maximum(masked_captcha_image, normalized_letter_image)
     min_pixel_values = np.minimum(masked_captcha_image, normalized_letter_image)
-
     actual_difference = sum(subtract(max_pixel_values, min_pixel_values))
     return actual_difference / max_possible_difference
