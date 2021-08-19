638a78991a8958037af08e9e65f45e533da4f1e5
basti <bastian.kubaile01@gmail.com>
2021-08-19 21:49:31 +0200

    Added multiprocessing to CaptchaSolver to utilize multiple CPUs.

--- src/CaptchaSolver.py
+++ src/CaptchaSolver.py
@@ -1,8 +1,9 @@
 import functools
+from multiprocessing import Process
 from src.ImageReader import read_image
 from src.LetterStore import LetterStore
 from src.DifferenceCalculator import get_differences, DifferencePositionList
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 LetterFits = Tuple[float, int, str]
 
@@ -12,29 +13,47 @@ class CaptchaSolver:
     def __init__(self,
                  letter_store: LetterStore,
                  letters_per_captcha: int = 6,
-                 possible_letter_duplicates_per_captcha: int = 2):
+                 possible_letter_duplicates_per_captcha: int = 2,
+                 processes_to_use: int = 4):
         self.letter_store: LetterStore = letter_store
         self.possible_duplicates: int = possible_letter_duplicates_per_captcha
         self.letters_per_captcha: int = letters_per_captcha
+        self.processes_to_use: int = processes_to_use
 
     def parse_image(self, img_path: str) -> str:
         captcha_image = read_image(img_path)
         all_positions: List[LetterFits] = []
 
+        letters_for_each_process = [{} for _ in range(self.processes_to_use)]
+        idx_process_to_use: int = 0
         for letter in self.letter_store.get_letters():
+            letters_for_each_process[idx_process_to_use][letter] = self.letter_store.get_images(letter)
+            idx_process_to_use = (idx_process_to_use + 1) % self.processes_to_use
 
-            all_differences: DifferencePositionList = []
-            for letter_image in self.letter_store.get_images(letter):
-                all_differences = sorted(get_differences(captcha_image, letter_image) + all_differences)
+        processes = [Process(target=self._parse_image_for_letters, args=(letters_for_each_process[i], captcha_image))
+                     for i in range(self.processes_to_use)]
 
-            useful_positions = self._find_useful_differences_positions(all_differences, letter_image.shape[1])
-            to_append: List[LetterFits] = list(map(lambda x: (x[0], x[1], letter), useful_positions))
-            all_positions = sorted(to_append + all_positions)
+        for process in processes:
+            process.start()
+        for process in processes:
+            process.join()
+        #TODO: Get return values of each process
         most_relevant_positions = all_positions[0: self.letters_per_captcha]
         sorted_by_x_offset = sorted(most_relevant_positions, key=lambda x:  x[1])
         captcha_string = functools.reduce(lambda reduced_value, position: reduced_value + position[2],
                                           sorted_by_x_offset, "")
         return captcha_string
+
+    def _parse_image_for_letters(self, letters: Dict[str, List], captcha_image):
+        all_positions: List[LetterFits] = []
+        for letter in letters.keys():
+            all_differences: DifferencePositionList = []
+            for letter_image in letters[letter]:
+                all_differences = sorted(get_differences(captcha_image, letter_image) + all_differences)
+            useful_positions = self._find_useful_differences_positions(all_differences, letter_image.shape[1])
+            to_append: List[LetterFits] = list(map(lambda x: (x[0], x[1], letter), useful_positions))
+            all_positions = sorted(to_append + all_positions)
+        return all_positions
 
     def _find_useful_differences_positions(self, raw_differences: DifferencePositionList, letter_width: int):
         useful_positions: DifferencePositionList = []
--- src/LetterStore.py
+++ src/LetterStore.py
@@ -6,7 +6,7 @@ from src.ImageReader import read_image
 class LetterStore:
 
     def __init__(self, letter_path):
-        self.letter_pixels = {}
+        self.letter_matrices = {}
 
         letter_dirs_and_names = [(letter_path + "/" + directory_name, directory_name)
                                  for directory_name in listdir(letter_path)
@@ -18,15 +18,15 @@ class LetterStore:
                             if isfile(letter_dir + "/" + letter_filename)]
 
             for letter_file in letter_files:
-                if letter_name in self.letter_pixels:
-                    self.letter_pixels[letter_name].append(read_image(letter_file))
+                if letter_name in self.letter_matrices:
+                    self.letter_matrices[letter_name].append(read_image(letter_file))
                 else:
-                    self.letter_pixels[letter_name] = [read_image(letter_file)]
+                    self.letter_matrices[letter_name] = [read_image(letter_file)]
 
     def get_letters(self):
-        return list(self.letter_pixels.keys())
+        return list(self.letter_matrices.keys())
 
     def get_images(self, letter: str):
-        if letter not in self.letter_pixels:
-            raise ValueError(f"There are no pixel matrices for the letter {letter}")
-        return self.letter_pixels[letter]
+        if letter not in self.letter_matrices:
+            raise ValueError(f"There are no image matrix for the letter {letter}")
+        return self.letter_matrices[letter]
