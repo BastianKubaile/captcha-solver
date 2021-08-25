4584b419605eb6c9230ef95ef65c3094cffbd3f1
basti <bastian.kubaile01@gmail.com>
2021-08-25 13:50:59 +0200

    Letter Store Test now covers more behaviour of letter store.

--- testing/TestLetterStore.py
+++ testing/TestLetterStore.py
@@ -1,6 +1,23 @@
+from os import listdir
+import pytest
 from src.LetterStore import LetterStore
 
+letter_directory = "data/letters"
 
-def test_letter_store():
-    underTest = LetterStore("data/letters")
-    assert False
+
+def test_gets_all_files():
+    under_test = LetterStore(letter_directory)
+
+    number_of_letter_directories = len(listdir(letter_directory))
+    assert len(under_test.get_letters()) == number_of_letter_directories
+
+    for letter in under_test.get_letters():
+        number_of_images = len(listdir(f"{letter_directory}/{letter}"))
+        assert len(under_test.get_images(letter)) == number_of_images
+
+
+def test_raises_value_error_on_unknown_letter():
+    under_test = LetterStore(letter_directory)
+
+    with pytest.raises(ValueError):
+        under_test.get_images("a")
